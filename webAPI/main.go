5f87ae91f3bc387b044d5e4344eedf7deef27713
Pranavi Roy <pranavyroy@gmail.com>
2020-06-08 18:44:43 +0530

    Added the function and handler to add new employees.

--- webAPI/main.go
+++ webAPI/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 //Creating a slice of employee objects
@@ -30,33 +32,50 @@ func returnData(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["Id"])
 	var index int = -1
-	for i := range company{
+	for i := range company {
 		index++
-		if company[i].Id == id{
+		if company[i].Id == id {
 			break
 		}
 	}
+	fmt.Println("Endpoint Hit: returnData")
 	fmt.Fprintln(w, "Employee data:")
 	json.NewEncoder(w).Encode(company[index])
 }
 
 //Function to fetch all employee data
-func returnAllData(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Endpoint Hit: returnData")
+func returnAllData(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllData")
 	json.NewEncoder(w).Encode(company)
+}
+
+//Function to add a new Employee
+func addEmployee(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// return the string response containing the request body
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var emp employee
+	json.Unmarshal(reqBody, &emp)
+	// update our global Articles array to include
+	// our new Article
+	company = append(company, emp)
+	fmt.Println("Endpoint Hit: addEmp")
+	fmt.Fprintln(w, "Added a new Employee!")
+	json.NewEncoder(w).Encode(emp)
 }
 
 func handleRequests() {
 	// creates a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
-    // replace http.HandleFunc with myRouter.HandleFunc
-    myRouter.HandleFunc("/", homePage)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/allEmpData", returnAllData).Methods("GET")
 	myRouter.HandleFunc("/empData/{id}", returnData).Methods("GET")
-    // finally, instead of passing in nil, we want
-    // to pass in our newly created router as the second
-    // argument
-    log.Fatal(http.ListenAndServe(":8080", myRouter))
+	myRouter.HandleFunc("/addEmp", addEmployee).Methods("POST")
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
