eaf380a7b80953204cb056dfae32f54386223ed5
Pranavi Roy <pranavyroy@gmail.com>
2020-06-08 18:33:11 +0530

    Added the function and function handler to fetch employee data.

--- webAPI/main.go
+++ webAPI/main.go
@@ -1,37 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "log"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"strconv"
 )
 
 //Creating a slice of employee objects
 var company []employee
 
 //Creating an employee struct to hold employee data
-type employee struct{
-	Id int `json:"Id"`
-	Name string `json:"Name"`
-	Role string `json:"Role"`
+type employee struct {
+	Id      int    `json:"Id"`
+	Name    string `json:"Name"`
+	Role    string `json:"Role"`
 	Address string `json:"Address"`
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnData(w http.ResponseWriter, r *http.Request){
+//Function to fetch employee data, given emp id
+func returnData(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["Id"])
+	var index int = -1
+	for i := range company{
+		index++
+		if company[i].Id == id{
+			break
+		}
+	}
+	fmt.Fprintln(w, "Employee data:")
+	json.NewEncoder(w).Encode(company[index])
+}
+
+//Function to fetch all employee data
+func returnAllData(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: returnData")
 	json.NewEncoder(w).Encode(company)
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/employeeData", returnData)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+    // replace http.HandleFunc with myRouter.HandleFunc
+    myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/allEmpData", returnAllData).Methods("GET")
+	myRouter.HandleFunc("/empData/{id}", returnData).Methods("GET")
+    // finally, instead of passing in nil, we want
+    // to pass in our newly created router as the second
+    // argument
+    log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
@@ -39,15 +64,15 @@ func main() {
 
 	company = []employee{
 		employee{
-		Id: 1, 
-		Name: "Sherlock Holmes", 
-		Role: "UI Designer", 
-		Address: "221B Baker Street"},
+			Id:      1,
+			Name:    "Sherlock Holmes",
+			Role:    "UI Designer",
+			Address: "221B Baker Street"},
 		employee{
-			Id: 2,
-			Name: "Sirius Black",
-			Role: "Project Manager",
+			Id:      2,
+			Name:    "Sirius Black",
+			Role:    "Project Manager",
 			Address: "12 Grimmauld Place"}}
 
-    handleRequests()
+	handleRequests()
 }
